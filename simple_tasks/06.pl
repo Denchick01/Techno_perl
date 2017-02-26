a562c853c12b37e049fa2493a1963dcf24957601
bird <bird@PowerPC>
2017-02-26 06:39:47 +0300

    tasks

--- simple_tasks/06.pl
+++ simple_tasks/06.pl
@@ -7,16 +7,15 @@ use 5.10.0;
 sub encode($$; @);
 sub decode($$; @);
 
-
 sub encode($$; @) {
     	my ($str, $key) = @_;
     	my @encoded_str = '';
+	my $temp_key = '';
 
-	
-	while ($key > 127 ) {
-		$key -= 127;
-	}
-	@encoded_str = join "", map {chr(ord($_) + $key)} split(//, $str);
+	@encoded_str = join "", map {chr(do {$temp_key = ord($_) + $key; 
+	while ($temp_key > 127 ) {$temp_key -= 127;} 
+	while ($temp_key < 0) {$temp_key += 127;} 
+	$temp_key})} split(//, $str);
 
     	say "@encoded_str";
 }
@@ -24,12 +23,12 @@ sub encode($$; @) {
 sub decode($$; @) {
     	my ($encoded_str, $key) = @_;
     	my @str = '';
+	my $temp_key = '';
 	
-	while ($key > 127) {
-		$key -= 127;
-	}
-	
-	@str = join "", map {chr(ord($_) - $key)} split(//, $encoded_str);
+	@str = join "", map {chr(do {$temp_key = ord($_) - $key; 
+	while ($temp_key > 127 ) {$temp_key -= 127;} 
+	while ($temp_key < 0) {$temp_key += 127;}
+	$temp_key})} split(//, $encoded_str);
 
     	say "@str";
 }
