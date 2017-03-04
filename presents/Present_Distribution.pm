7a0ac22fef66cf0af27567e71dfb0ccc0ed91d87
bird <bird@PowerPC>
2017-03-04 19:29:13 +0300

    Исправил 2 задание

--- presents/Present_Distribution.pm
+++ presents/Present_Distribution.pm
@@ -31,26 +31,24 @@ sub present_distribution(@) {
 	}
 
 	$number_of_people = keys %people;
-	#С каждым участником ассоциирован счетчик, значение которого зависит
-	#от того является участник дарителем или дарящим 
 	%to_pr = map {$_ => 0} sort { int (rand (3) - 1) } keys %people; 
 									 								
 	TRY_AGAIN:
 	for my $from_m  ( keys %people) {
 		for my $to_m (keys %to_pr) {
 			#Проверка на то, что дарящий и даритель одно и тоже лицо
-			next if ($from_m =~ m/^$to_m$/i); 
+			next if ($from_m eq $to_m); 
 			#Проверка на то, что дарящий и даритель супруги
-			next if ($people{$from_m}->[0] && $people{$from_m}->[0] =~ m/^$people{$to_m}->[0]$/g);
+			next if ($people{$from_m}->[0] && $people{$from_m}->[0] eq $people{$to_m}->[0]);
 			#Проверка, что дарящий не получал подарка от дарителя
-			next if ($people{$from_m}->[1] && $people{$from_m}->[1] =~ m/^$to_m$/g);
+			next if ($people{$from_m}->[1] && $people{$from_m}->[1] eq $to_m);
 			push @res_pr, [$from_m, $to_m];
 			$people{$to_m}->[1] = $from_m;
 			delete $to_pr{$to_m};
 			last;		
 		}
 	}
-	goto TRY_AGAIN if (@res_pr != $number_of_people);
+	goto TRY_AGAIN if (int(@res_pr/2) != int($number_of_people/2));
 
 	return @res_pr;
 }
