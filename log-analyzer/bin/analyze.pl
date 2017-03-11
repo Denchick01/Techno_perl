5114f5e61662ad5b684a015c2bb8b2254c0b6cbc
bird <bird@PowerPC>
2017-03-12 01:57:20 +0300

    task 5

--- log-analyzer/bin/analyze.pl
+++ log-analyzer/bin/analyze.pl
@@ -20,26 +20,28 @@ sub parse_file {
     while (my $log_line = <$fd>) {
         $log_line =~ m{(?<IP>.+?)\s+\[(?<TIME>.*):(.*)\]\s+"((.)*?)"\s+(?<CODE>\d+)\s+(?<SIZE>\d+)\s+"(.+)"\s+"(?<K>.+?)"\s+};
 
+        if (!exists $log_info{TOTAL_TIME}{$+{TIME}}) {
+            $log_info{TOTAL_TIME}{$+{TIME}} = 1;
+        }
 	if (!exists $log_info{$+{IP}}) { 
             $log_info{$+{IP}} = {IP => $+{IP}, COUNT => 0, AVG => 0, DATA => 0,  DATA_200 => 0,
                                  DATA_301 => 0, DATA_302 => 0, DATA_400 => 0, DATA_403 => 0,    
                                  DATA_404 => 0, DATA_408 => 0, DATA_414 => 0, DATA_499 => 0,   
-                                 DATA_500 => 0, TIME => {}, N_MIN => 0};
+                                 DATA_500 => 0, TIME => {}};
 	}
 	++$log_info{$+{IP}}{COUNT};         
         if ($+{CODE} == 200) {
 		my $temp_k = $+{K};
 		$temp_k = 0 if ($+{K} eq "-");
-        	$log_info{$+{IP}}{DATA} += $+{SIZE} * $temp_k/1024;
+        	$log_info{$+{IP}}{DATA} += $+{SIZE} * $temp_k;
 	}
         $log_info{$+{IP}}{"DATA_$+{CODE}"} += $+{SIZE}/1024;
 
         #Подсчет минут и AVG 
         if (! exists $log_info{$+{IP}}{TIME}{$+{TIME}}) {
-            $log_info{$+{IP}}{N_MIN}++;
             $log_info{$+{IP}}{TIME}{$+{TIME}} = 1;
         }
-        $log_info{$+{IP}}{AVG} = $log_info{$+{IP}}{COUNT}/$log_info{$+{IP}}{N_MIN};     
+        $log_info{$+{IP}}{AVG} = $log_info{$+{IP}}{COUNT}/keys %{$log_info{$+{IP}}{TIME}};     
     }                   
     
     close $fd;
@@ -50,11 +52,18 @@ sub parse_file {
 sub report {
     my $result = shift;
     my %total_info = ();
-    for my $key_ip (keys %$result) {
-        map {$total_info{$_} += ${$result->{$key_ip}}{$_}} grep {$_ ne "TIME" && $_ ne "N_MIN" && $_ ne "IP"} keys %{$result->{$key_ip}};
-    }
+
     #Сортировка по count
-    my @sort_keys = sort { $result->{$b}{COUNT} <=> $result->{$a}{COUNT} } keys %$result;
+    my @sort_keys = sort { $result->{$b}{COUNT} <=> $result->{$a}{COUNT} }
+                    grep {$_ ne "TOTAL_TIME"}  keys %$result;
+
+    for my $key_ip (@sort_keys) {
+        map {$total_info{$_} += ${$result->{$key_ip}}{$_}} 
+        grep {$_ ne "TIME" && $_ ne "IP" && $_ ne "AVG"} 
+        keys %{$result->{$key_ip}};
+    }
+
+    $total_info{AVG} = $total_info{COUNT}/keys %{$result->{TOTAL_TIME}};
 
     printf "%-20s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-10s\n", 
            "IP", "count", "avg", "data", "data_200", "data_301", "data_302", "data_400", 
@@ -67,7 +76,7 @@ sub report {
     for (my $it = 0; $it < 10; ++$it) {
 
         printf "%-20s%-10.1d%-10.2f%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d%-10.1d\n",
-               @{$result->{$sort_keys[$it]}}{"IP", "COUNT", "AVG", "DATA", "DATA_200", "DATA_301", 
+               @{$result->{$sort_keys[$it]}}{"IP", "COUNT", "AVG"}, ${$result->{$sort_keys[$it]}}{"DATA"}/1024, @{$result->{$sort_keys[$it]}}{"DATA_200", "DATA_301", 
 	       "DATA_302", "DATA_400", "DATA_403", "DATA_404", "DATA_408", "DATA_414", "DATA_499", "DATA_500"};
     }
 
